f1e731cba4df0cc9fb339c60dbc72586d527638c
David Arranz González <a25davidag@teis.intranet>
2025-09-16 19:06:14 +0200

    while3

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,32 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in)
+        Scanner sc = new Scanner(System.in);
         // Ejercicio 5
         System.out.println("Ingresa un numero");
         int n1 = sc.nextInt();
-        while
-
-
-
+        int i = 1;
+        while (i <= n1) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+            i++;
         }
+
+        // Ejercicio 6
+
+
+        System.out.println("Ingresa un numero");
+        int n2 = sc.nextInt();
+        int secreto = 20;
+        while (n2 == secreto) {
+            if (n2 == secreto) {
+                System.out.println("El numero es correcto");
+            }else if (n2 != secreto){
+                System.out.println("El numero no es correcto");
+            }
+            n2++;
+            sc.close();
+        }
+    }
     }
