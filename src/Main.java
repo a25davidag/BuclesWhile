52ba52f75460d8cf450e3df9a67fb383c2e49782
dabid chu <arranzd5@gmail.com>
2025-09-19 00:04:33 +0200

    BUcles primera parte

--- src/Main.java
+++ src/Main.java
@@ -27,12 +27,74 @@ public class Main {
             intentos = sc.nextInt();
             if (intentos < n2) {
                 System.out.println("Numero bajo intentalo de nuevo");
-            }else if (intentos > n2){
+            } else if (intentos > n2) {
                 System.out.println("Numero alto intentalo de nuevo");
-            }else {
+            } else {
                 System.out.println("Bien");
             }
         }
-        }
-    }
 
+
+        //Ejercicio7
+        Scanner st = new Scanner(System.in);
+        int opcion;
+
+        do {
+            System.out.println("===== MENÚ PRINCIPAL =====");
+            System.out.println("1. Opción 1");
+            System.out.println("2. Opción 2");
+            System.out.println("3. Opción 3");
+            System.out.println("0. Salir");
+            System.out.print("Elige una opción: ");
+            opcion = st.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    System.out.println("Has elegido la Opción 1");
+                    break;
+                case 2:
+                    System.out.println("Has elegido la Opción 2");
+                    break;
+                case 3:
+                    System.out.println("Has elegido la Opción 3");
+                    break;
+                case 0:
+                    System.out.println("Saliendo del programa...");
+                    break;
+                default:
+                    System.out.println("Opción no válida, intenta de nuevo.");
+            }
+
+        } while (opcion != 0);
+
+        //Ejercicio 8
+        Scanner sc = new Scanner(System.in);
+        int numero, suma = 0;
+
+        do {
+            System.out.print("Ingresa un número (0 Para finalizar suma): ");
+            numero = sc.nextInt();
+            suma = suma + numero;
+
+        } while (numero != 0);
+
+        System.out.println("La suma total es: " + suma);
+
+
+        //Ejercicio 9
+        Scanner sj = new Scanner(System.in);
+        System.out.println("Validacion de contraseña");
+        System.out.println("Introduzca una contraseña");
+        String contraseñaCorrecta = sj.nextLine();
+        System.out.println("Vuelva a introducirla");
+        String contraseñaIngresada = sj.nextLine();
+        while (!contraseñaIngresada.equals(contraseñaCorrecta)){
+            System.out.println("Contraseña incorrecta, Intentalo de nuevo ");
+            contraseñaIngresada = sj.nextLine();
+            if(!contraseñaIngresada.equals(contraseñaCorrecta)){
+                //System.out.println("Contraseña incorrecta, Intentalo de nuevo");
+            }
+        }
+        System.out.println("Acesso concedido");
+    }
+}
